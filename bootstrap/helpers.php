bc9607a5f3038cac6bdcbcd621331e0592ba5bf4
ollbao <zhongjinb@foxmail.com>
2017-11-06 10:09:22 +0800

    首页创建

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,2 +1,10 @@
 <?php
-require __DIR__ . 'helpers.php';
+
+/**
+ * 将当前请求的路由名称转换为 CSS 类名称，作用是允许我们针对某个页面做页面样式定制
+ * @return string
+ */
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
